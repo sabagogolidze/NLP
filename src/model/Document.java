bb2edd16fe43c1ba01c2c088acdb95d5e9f46c2e
saba <sggol11@freeuni.edu.ge>
2014-07-04 19:03:16 +0400

    bla

--- src/model/Document.java
+++ src/model/Document.java
@@ -1,5 +1,44 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Document {
+
+	private HashMap<Topic, Integer> topicCount;
+	private ArrayList<Word> words;
+
+	public Document(ArrayList<Word> words) {
+		this.words = words;
+		topicCount = new HashMap<Topic, Integer>();
+	}
+
+	public int getSize() {
+		return words.size();
+	}
+
+	public ArrayList<Word> getWords() {
+		return words;
+	}
+
+	public void increaseTopicCount(Topic t) {
+		if (topicCount.containsKey(t)) {
+			topicCount.put(t, topicCount.get(t) + 1);
+		} else {
+			topicCount.put(t, 1);
+		}
+	}
+
+	public void decreaseTopicCount(Topic t) {
+		topicCount.put(t, topicCount.get(t) - 1);
+	}
+	
+	public int getTopicCount(Topic t){
+		if (topicCount.containsKey(t)) {
+			return topicCount.get(t);
+		} else {
+			return 0;
+		}
+	}
 
 }
--- src/model/Topic.java
+++ src/model/Topic.java
@@ -1,5 +1,18 @@
 package model;
 
 public class Topic {
+	private int wordCount = 0;
+
+	public int getWordCount() {
+		return wordCount;
+	}
+
+	public void increaseWordCount() {
+		wordCount++;
+	}
 	
+	public void decreaseWordCount() {
+		wordCount--;
+	}
+
 }
