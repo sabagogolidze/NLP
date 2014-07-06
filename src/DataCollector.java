158b57ec9790a2ead1ccf5d7c852cf8a08d9f3f9
giorgi-matiashvili <gmati11@freeuni.edu.ge>
2014-07-06 14:34:55 +0400

    data collector class and training data added

---
+++ src/DataCollector.java
@@ -0,0 +1,38 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+
+public class DataCollector {
+	
+	
+	public static void main(String[] args) throws IOException {
+		String content = new Scanner(new File("rawData.txt")).useDelimiter("\\Z").next();
+		content = content.replaceAll("[^ა-ჰ\\d -.!?_,:;\"']", " ");
+		content = content.replaceAll("\\(", " ");
+		content = content.replaceAll("\\)", " ");
+
+		content = content.replaceAll("\\.+", ".");
+		content = content.replaceAll("[.!?;]", ".");
+		content = content.replaceAll("_", "");
+		content = content.replaceAll(" +", " ");
+		content = content.replaceAll("\\. +", ".");
+		content = content.replaceAll("\\.+", ".");
+		content = content.replaceAll("\\.", ".\n");
+
+		content = content.replaceAll("([^ა-ჰ])(-)", " ");
+
+		content = content.replaceAll("\\. +", ".");
+		content = content.replaceAll("\\.", ".\n");
+		content = content.replaceAll("\n+", "\n");
+		content = content.replaceAll("(-)([^ა-ჰ])", " ");
+		
+
+		PrintWriter wr = new PrintWriter(new FileWriter("output.txt"));
+		wr.append(content + "\n");
+		wr.close();
+	}
+}
