81096635c9536c0d69a9e412c58f5a2965789244
giorgi-matiashvili <gmati11@freeuni.edu.ge>
2014-07-06 04:09:48 +0400

    added evaluator

---
+++ src/Evaluator.java
@@ -0,0 +1,22 @@
+import java.util.Map;
+
+import model.Topic;
+
+
+public class Evaluator {
+	
+	public Evaluator(){
+		train();
+	}
+	
+	
+	private void train() {
+		// TODO Auto-generated method stub
+	}
+
+
+	public int evaluate(int spellMistakes, Map<Topic, Double> topicRelativity){
+		return 0;
+	}
+
+}
